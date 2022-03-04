e8f4edc5a38184be184d2ad9dbe293d73d660f7e
lz已经是条咸鱼了 <xibaole2333@gmail.com>
2022-03-05 03:08:39 +0800

    test url preview

--- api/feed.go
+++ api/feed.go
@@ -8,6 +8,16 @@ import (
 	"strings"
 )
 
+var html = `
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>酷安动态</title>
+<meta name="description" content="%s">
+<meta property="og:title" content="酷安动态">
+<link rel="icon" sizes="any" mask="" href="https://static.coolapk.com/static/web/v8/images/header-logo.png">
+</head>
+`
+
 func UrlHandler(w http.ResponseWriter, r *http.Request) {
 	if len(r.URL.Path) <= 1 {
 		_, _ = fmt.Fprintf(w, "Invaid Feed ID")
@@ -20,10 +30,13 @@ func UrlHandler(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "Internal Error")
 		return
 	}
-	if feedDetail.Data.ShareUrl != "" {
-		http.Redirect(w, r, feedDetail.Data.ShareUrl, http.StatusMovedPermanently)
-	} else {
+	if feedDetail.Data.ShareUrl == "" {
 		_, _ = fmt.Fprintf(w, "Invaid Feed ID")
+	}
+	if strings.Contains(r.UserAgent(), "bot") && strings.Contains(r.UserAgent(), "Bot") {
+		_, _ = fmt.Fprintf(w, fmt.Sprintf(html, feedDetail.Data.Message))
+	} else {
+		http.Redirect(w, r, feedDetail.Data.ShareUrl, http.StatusMovedPermanently)
 	}
 	return
 }
