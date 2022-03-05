1faf824a59abe707b1cd683c3bf4698114a38bfb
lz已经是条咸鱼了 <xibaole2333@gmail.com>
2022-03-05 17:27:57 +0800

    test logo

--- api/feed.go
+++ api/feed.go
@@ -15,7 +15,7 @@ var html = `
 <title>酷安动态</title>
 <meta name="description" content="%s">
 <meta property="og:title" content="酷安动态">
-<link rel="icon" sizes="any" mask="" href="https://static.coolapk.com/static/web/v8/images/header-logo.png">
+<link rel="shortcut icon" href="https://static.coolapk.com/static/web/v8/images/header-logo.png">
 </head>
 `
 
