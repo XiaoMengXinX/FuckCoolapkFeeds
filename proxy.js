d227e20e822be22fd90a1b36cca1a3dab97849ed
xiaomengxin <xibaole2333@gmail.com>
2025-11-09 03:17:52 +0800

    fix: rename middleware to proxy

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
+export function proxy(request) {
   const { pathname, search } = request.nextUrl;
 
   const pathMatch = pathname.match(/^\/(feed|picture|iv)\/([^/]+)$/);
