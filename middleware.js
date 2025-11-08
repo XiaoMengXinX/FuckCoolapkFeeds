de797de5a7693193fbebcc504d7dd44b88304dc7
xiaomengxin <xibaole2333@gmail.com>
2025-11-09 03:13:45 +0800

    fix: bypass cdn bug

--- middleware.js
+++ middleware.js
@@ -3,12 +3,39 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   const { pathname, search } = request.nextUrl;
 
-  const pathRegex = /^\/(feed|picture|iv)\/[^/]+$/;
+  const pathMatch = pathname.match(/^\/(feed|picture|iv)\/([^/]+)$/);
   
-  if (pathRegex.test(pathname) && search) {
-    const url = request.nextUrl.clone();
-    url.search = '';
-    return NextResponse.redirect(url, 301);
+  if (pathMatch) {
+    const [, routeType, id] = pathMatch;
+    const idAsNumber = parseInt(id, 10);
+    
+    if (isNaN(idAsNumber) || idAsNumber.toString() !== id.replace(/^0+/, '')) {
+      const url = request.nextUrl.clone();
+      url.pathname = '/';
+      url.search = '';
+      return NextResponse.redirect(url, 301);
+    }
+    
+    let needsRedirect = false;
+    let normalizedId = id;
+
+    const normalizedIdFromNumber = idAsNumber.toString();
+    if (normalizedIdFromNumber !== id) {
+      normalizedId = normalizedIdFromNumber;
+      needsRedirect = true;
+    }
+
+    if (search) {
+      needsRedirect = true;
+    }
+
+    if (needsRedirect) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/${routeType}/${normalizedId}`;
+      url.search = '';
+      
+      return NextResponse.redirect(url, 301);
+    }
   }
 
   return NextResponse.next();
